0d0565ff0ca2fb1f608fb6e7d53038959319f1d0
Sean R Reid <sean@torchcodelab.com>
2021-03-02 11:59:24 -0500

    Initial commit

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import LoginMessage from "./LoginMessage";
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      serverResponse: null
+    };
+  }
+
+  _onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  _handleSubmit = (event) => {
+    event.preventDefault();
+    const serverResponse = this.props.handleSubmit(
+      this.state.username,
+      this.state.password
+    );
+    this.setState(
+      {
+        serverResponse
+      },
+      () => {
+        console.log("server response is:", serverResponse);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this._handleSubmit}>
+          <label>
+            Username:
+            <input
+              type="text"
+              name="username"
+              placeholder="Your Username"
+              value={this.state.username}
+              onChange={(event) => {
+                this._onChange(event);
+              }}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              placeholder="Your Password"
+              value={this.state.password}
+              onChange={(event) => {
+                this._onChange(event);
+              }}
+            />
+          </label>
+          <button type="submit">Login</button>
+        </form>
+        {!!this.state.serverResponse ? (
+          <LoginMessage {...this.state.serverResponse} />
+        ) : null}
+      </>
+    );
+  }
+}
+
+export default LoginForm;
